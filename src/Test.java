6350306010f32bdef65dcf534493852636e62aaf
Albert Dominguez <albert.j.dominguez21@gmail.com>
2021-09-03 10:47:45 -0500

    syntax types and variables

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,27 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int myFavoriteNumber = 13;
+        System.out.println(myFavoriteNumber);
+        String myString = "Hello Draco";
+        System.out.println(myString);
+        float myNumber = 3;
+        System.out.println(myNumber);
+/*
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+        String theNumberThree = "three";
+        Object o = theNumberThree;
+        int three = (int) o;
+*/
+//        int x = 4;
+//        x += 5;
+        int x = 3;
+        int y = 4;
+        x *= y;
+        System.out.println(x);
     }
 }
