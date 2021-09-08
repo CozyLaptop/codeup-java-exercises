5a090767ae4e8ba2786be12051448a623a895e2a
CozyLaptop <albert.j.dominguez21@gmail.com>
2021-09-08 12:16:53 -0500

    small refactor

--- src/MethodsMiniExercises.java
+++ src/MethodsMiniExercises.java
@@ -5,17 +5,17 @@ public class MethodsMiniExercises {
     public static void main(String[] args) {
         seeIfItMatches();
     }
-    private static String stringInput(Scanner sc){
-        System.out.println("Please enter an input.");
+    private static String askForString(Scanner sc){
+        System.out.println("Please enter a word.");
         return sc.nextLine();
     }
     private static int askForInt(Scanner sc){
-        System.out.println("Please enter an integer.");
+        System.out.println("Please enter an number.");
         return sc.nextInt();
     }
     private static void seeIfItMatches(){
         Scanner sc = new Scanner(System.in);
-        if (stringInput(sc).length() == askForInt(sc)) {
+        if (askForString(sc).length() == askForInt(sc)) {
             System.out.println("That matches the length of the input!");
         } else {
             System.out.println("That doesn't match the length of the input!");
