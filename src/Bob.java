f151a5f38d6652e3dbbd353acfebf5dc3c989add
CozyLaptop <albert.j.dominguez21@gmail.com>
2021-09-08 10:20:47 -0500

    finished partial exercise

--- src/Bob.java
+++ src/Bob.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Bob {
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args) {
+        System.out.println("How are you doing today? (Good or bad?)");
+        Scanner scanner = new Scanner(System.in);
+        String answer = scanner.nextLine().toLowerCase();
+        if (answer.endsWith("?")) {
+            System.out.println("Sure");
+        } else if (answer.endsWith("!")) {
+            System.out.println("Woah, chill out!");
+        } else if (answer.equals(" ")) {
+            System.out.println("Fine be that way!");
+        } else {
+            System.out.println("Whatever");
+        }
+    }
 }
