ec495878ea0d4e45e694c2be7a3cbc8e8d227384
CozyLaptop <albert.j.dominguez21@gmail.com>
2021-09-08 11:52:09 -0500

    Bob complete.

--- src/Bob.java
+++ src/Bob.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 
 public class Bob {
     public static void main(String[] args) {
-        System.out.println("How are you doing today? (Good or bad?)");
+        System.out.println("Ask Bob a question");
         Scanner scanner = new Scanner(System.in);
         String answer = scanner.nextLine().toLowerCase();
         if (answer.endsWith("?")) {
             System.out.println("Sure");
         } else if (answer.endsWith("!")) {
             System.out.println("Woah, chill out!");
-        } else if (answer.equals(" ")) {
+        } else if (answer.isEmpty() | answer.equals("")) {
             System.out.println("Fine be that way!");
         } else {
             System.out.println("Whatever");
