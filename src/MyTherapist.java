3af6c7755657e6cc421a3aa3ff2f4cf1793b4571
CozyLaptop <albert.j.dominguez21@gmail.com>
2021-09-08 09:16:22 -0500

    finished therapist exercise

--- src/MyTherapist.java
+++ src/MyTherapist.java
@@ -2,7 +2,18 @@ import java.util.Scanner;
 
 public class MyTherapist {
     public static void main(String[] args) {
-        System.out.println("How are you doing today?");
-        Scanner scanner = new Scanner(System.in);
+        String feeling;
+        do {
+            System.out.println("How are you doing today? (Good or bad?)");
+            Scanner scanner = new Scanner(System.in);
+            feeling = scanner.nextLine().toLowerCase();
+            if (feeling.equals("good")) {
+                System.out.println("Great, keep it up!");
+            } else if (feeling.equals("bad")) {
+                System.out.println("Not a good answer, try again");
+            } else {
+                System.out.println("I don't know what that is");
+            }
+        } while (!feeling.equals("good")) ;
     }
 }
