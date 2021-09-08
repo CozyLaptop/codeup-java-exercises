2e78c1dac98a4db1d82f7f4f3e47275ad3bf6cf4
CozyLaptop <albert.j.dominguez21@gmail.com>
2021-09-08 09:07:49 -0500

    init

---
+++ src/MyTherapist.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class MyTherapist {
+    public static void main(String[] args) {
+        System.out.println("How are you doing today?");
+        Scanner scanner = new Scanner(System.in);
+    }
+}
