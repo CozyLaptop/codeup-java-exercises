0b1278e6bc0df87b47d8b80de7b48488d3d9866d
CozyLaptop <albert.j.dominguez21@gmail.com>
2021-09-09 15:13:57 -0500

    finished #3

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,4 +1,8 @@
+import java.util.Scanner;
+
 public class MethodsExercises {
+    public static Scanner sc = new Scanner(System.in);
+
     private static int addition(int num1, int num2){
         return num1 + num2;
     }
@@ -14,11 +18,41 @@ public class MethodsExercises {
     private static int modulus(int num1, int num2){
         return num1 % num2;
     }
+    private static int getInteger(int min, int max){
+        System.out.print("Enter a number between " + min + " and " + max + " ");
+        int input = sc.nextInt();
+        if (input >= min && input <= max){
+            return input;
+        }
+        return getInteger(min, max);
+    }
+    private static void factorial(){
+        System.out.print("Enter a number between 1 and 10: ");
+        int input = sc.nextInt();
+        System.out.print(input + "! = ");
+        System.out.print("1");
+        if (input > 1){
+            createFactorial(1, input, 1);
+        }
+    }
+
+    private static void createFactorial(int currentNum, int inputNum, int result) {
+        currentNum = currentNum + 1;
+        result *= currentNum;
+        System.out.print(" x " + currentNum);
+        if (currentNum < inputNum){
+            createFactorial(currentNum, inputNum, result);
+        } else System.out.println(" Result: " + result);
+    }
+
     public static void main(String[] args) {
-        System.out.println(addition(10, 20));
-        System.out.println(subtraction(10, 20));
-        System.out.println(multiplication(10, 20));
-        System.out.println(division(10, 20));
-        System.out.println(modulus(10, 20));
+//        System.out.println(addition(10, 20));
+//        System.out.println(subtraction(10, 20));
+//        System.out.println(multiplication(10, 20));
+//        System.out.println(division(10, 20));
+//        System.out.println(modulus(10, 20));
+//        int userInput = getInteger(1, 10);
+//        System.out.println(userInput);
+        factorial();
     }
 }
