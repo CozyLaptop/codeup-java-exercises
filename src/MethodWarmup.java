72847c2bd8fe27f813c6d3f526eae9b57b0946bb
CozyLaptop <albert.j.dominguez21@gmail.com>
2021-09-09 09:20:06 -0500

    finished second exercise

---
+++ src/MethodWarmup.java
@@ -0,0 +1,11 @@
+public class MethodWarmup {
+    private static int getDifference(int num1, int num2){
+        return num1 - num2;
+    }
+    private static int getDifference(int num1){
+        return num1;
+    }
+    public static void main(String[] args) {
+
+    }
+}
