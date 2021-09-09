a33d09e179c6c01f3c9a12372d5374f4891e2409
CozyLaptop <albert.j.dominguez21@gmail.com>
2021-09-09 09:21:59 -0500

    refactor to use MAth.max

--- src/MethodWarmup.java
+++ src/MethodWarmup.java
@@ -1,6 +1,7 @@
 public class MethodWarmup {
     private static int getDifference(int num1, int num2){
-        return num1 - num2;
+
+        return Math.max(num1, num2) - Math.min(num1, num2);
     }
     private static int getDifference(int num1){
         return num1;
