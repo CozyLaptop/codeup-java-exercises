242ab4786bdd9ed8fc8e61595c9730d7bee27fbe
CozyLaptop <albert.j.dominguez21@gmail.com>
2021-09-09 11:08:47 -0500

    complete warmup

--- src/MethodWarmup.java
+++ src/MethodWarmup.java
@@ -1,12 +1,20 @@
 public class MethodWarmup {
+//    Gets the difference of the highest number and the lowest number
     private static int getDifference(int num1, int num2){
-
         return Math.max(num1, num2) - Math.min(num1, num2);
     }
+//    Gets difference of the number and 0. Can take in to account negative numbers if 0 is higher
     private static int getDifference(int num1){
-        return num1;
+        return Math.max(num1, 0) - Math.min(num1, 0);
     }
-    public static void main(String[] args) {
+//    Gets difference in double format
+    private static double getDifference(double double1, double double2){
+        return Math.max(double1, double2) - Math.min(double1, double2);
+    }
 
+    public static void main(String[] args) {
+        System.out.println(getDifference(2, 10));
+        System.out.println(getDifference(-6));
+        System.out.println(getDifference(5.5, 4.1));
     }
 }
