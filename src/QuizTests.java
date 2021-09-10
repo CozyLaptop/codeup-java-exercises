54d0a1a19fb4245dd99e4a843e78c7bb1fca641d
CozyLaptop <albert.j.dominguez21@gmail.com>
2021-09-10 10:17:19 -0500

    init

---
+++ src/QuizTests.java
@@ -0,0 +1,8 @@
+class QuizTests {
+    public static void main(String[] args) {
+        String name = "1";
+        int num1 = 1;
+        name  = name + num1;
+        System.out.println(name);
+    }
+}
