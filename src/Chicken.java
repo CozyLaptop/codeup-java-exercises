91cac8a6a5be51ff98ea4f6f88f61e2d315fe9ed
CozyLaptop <albert.j.dominguez21@gmail.com>
2021-09-10 14:17:34 -0500

    used getters and setters

--- src/Chicken.java
+++ src/Chicken.java
@@ -1,13 +1,34 @@
 public class Chicken {
-    String name;
-    String sound;
+    private String name;
+    private String sound;
     int eggCount;
     public void makeNoise(){
-        System.out.println(name + " says: " + Quote.randomQuote());
+//        System.out.println(name + " says: " + Quote.randomQuote());
+        System.out.println(sound);
     }
     public void layEgg(){
         System.out.println("You laid an egg");
         eggCount++;
         System.out.println("You have " + eggCount + " eggs.");
+    }
+    public Chicken(String name, String sound){
+        this.name = name;
+        this.sound = sound;
+        System.out.println(sound);
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setSound(String sound){
+        this.sound = sound;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSound() {
+        return sound;
     }
 }
--- src/TestChicken.java
+++ src/TestChicken.java
@@ -1,8 +1,9 @@
 public class TestChicken {
     public static void main(String[] args) {
-        Chicken lilChicken = new Chicken();
-        lilChicken.name = "Booster";
-        lilChicken.sound = "Ba kah!";
+        Chicken lilChicken = new Chicken("Booster", "Bakah!");
+//        Chicken should make it's noise here
+//        lilChicken.name = "Booster";
+//        lilChicken.sound = "Ba kah!";
         lilChicken.makeNoise();
         lilChicken.makeNoise();
         lilChicken.makeNoise();
