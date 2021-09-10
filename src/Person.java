3d4f35ff4c9fb41fbe3bb766a25027360cb21224
CozyLaptop <albert.j.dominguez21@gmail.com>
2021-09-10 15:16:43 -0500

    finished person test

---
+++ src/Person.java
@@ -0,0 +1,23 @@
+public class Person {
+    private String name;
+    public String getName(){
+        return name;
+//TODO: return the person's name
+    }
+
+    public void setName(String name){
+        this.name = name;
+//TODO: change the name property to the passed value
+    }
+    public void sayHello(){
+        System.out.println("Hello, " + name);
+//TODO: print a message to the console using the person's name
+
+    }
+    public Person (){
+
+    }
+    public Person (String name){
+        this.name = name;
+    }
+}
