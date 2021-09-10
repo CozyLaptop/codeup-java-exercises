a36826f76ac6aa2c02817095446d85bb7f730091
CozyLaptop <albert.j.dominguez21@gmail.com>
2021-09-10 10:28:19 -0500

    chicken lays eggs

--- src/Chicken.java
+++ src/Chicken.java
@@ -1,7 +1,13 @@
 public class Chicken {
     String name;
     String sound;
+    int eggCount;
     public void makeNoise(){
         System.out.println(name + " makes the sound " + sound);
+    }
+    public void layEgg(){
+        System.out.println("You laid an egg");
+        eggCount++;
+        System.out.println("You have " + eggCount + " eggs.");
     }
 }
---
+++ src/TestChicken.java
@@ -0,0 +1,11 @@
+public class TestChicken {
+    public static void main(String[] args) {
+        Chicken lilChicken = new Chicken();
+        lilChicken.name = "Booster";
+        lilChicken.sound = "Ba kah!";
+        lilChicken.makeNoise();
+        lilChicken.layEgg();
+        lilChicken.layEgg();
+        lilChicken.layEgg();
+    }
+}
