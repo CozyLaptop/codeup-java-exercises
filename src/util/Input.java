9362c1e3dfbf2479003136a1ea393af685a5e574
CozyLaptop <albert.j.dominguez21@gmail.com>
2021-09-14 10:47:36 -0500

    refactor to not display message every time getString is called

--- src/util/Input.java
+++ src/util/Input.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Input {
     private Scanner scanner;
     public String getString(){
-        System.out.println("Please enter a string: ");
+//        System.out.println("Please enter a string: ");
         return scanner.nextLine();
     }
     public Input(){
