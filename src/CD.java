501a293ceb142beb816bda936807a9e98756144c
CozyLaptop <albert.j.dominguez21@gmail.com>
2021-09-17 09:40:22 -0500

    Can extend CD off of media Uses an abstract class

---
+++ src/CD.java
@@ -0,0 +1,8 @@
+public class CD extends Media{
+    public void consume() {
+        System.out.println("You listen to the CD in your CD player.");
+    }
+    public void sellMedia(){
+        System.out.println("You sell the CD at a local CD shop");
+    }
+}
---
+++ src/Media.java
@@ -0,0 +1,7 @@
+public abstract class Media {
+    public void buyMedia(){
+        System.out.println("Thank you for buying me");
+    }
+    public abstract void consume();
+    public abstract void sellMedia();
+}
