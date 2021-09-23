8fc6f7ce27c281ca5f6a4e4c6cf83c2ba7d74af1
CozyLaptop <albert.j.dominguez21@gmail.com>
2021-09-23 09:46:18 -0500

    test

---
+++ src/Exceptions.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.Arrays;
+import java.util.List;
+
+public class Exceptions {
+    public static void main(String[] args) {
+        Path pathToOurDataDir = Paths.get("src/data");
+        System.out.println(pathToOurDataDir);
+        System.out.println();
+        Path pathToOurFile = Paths.get("src/data", "list.txt");
+        List<String> food = Arrays.asList("Milk", "Eggs", "Candy");
+        try {
+            Files.createDirectories(pathToOurDataDir);
+            if(Files.notExists(pathToOurFile)){
+                Files.createFile(pathToOurFile);
+            } else {
+                System.out.println("The " + pathToOurFile + " file already exists");
+            }
+        } catch (IOException ioe){
+            ioe.printStackTrace();
+        }
+        try {
+            Files.write(pathToOurFile, food);
+        } catch (IOException ioe){
+            ioe.printStackTrace();
+        }
+    }
+}
