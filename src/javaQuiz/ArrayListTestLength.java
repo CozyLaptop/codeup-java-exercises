7864721f6c84ad73e0f1888023f6dc0381a2954e
CozyLaptop <albert.j.dominguez21@gmail.com>
2021-09-30 11:31:10 -0500

    quiz work

---
+++ src/javaQuiz/ArrayListTestLength.java
@@ -0,0 +1,13 @@
+package javaQuiz;
+
+import java.util.ArrayList;
+
+public class ArrayListTestLength {
+    public static void main(String[] args) {
+        ArrayList<Integer> nums = new ArrayList<>();
+        nums.size();
+    }
+
+
+
+}
