e24639c023184a03579539c77dfb2ca7c7db606c
CozyLaptop <albert.j.dominguez21@gmail.com>
2021-10-14 09:56:08 -0500

    no string builder

--- src/JavaDrills.java
+++ src/JavaDrills.java
@@ -1,12 +1,18 @@
-// Work in progress
+
+// Work in progress flip first and last character
 public class JavaDrills {
     public static String flipOuterCase(String str){
+        String newString = "";
         for (int i = 0; i < str.length(); i++){
             char c = str.charAt(i);
             if (Character.isUpperCase(c)){
-                Character.toString();
-            }
+                newString += Character.toString(c).toLowerCase();
+            } else newString += Character.toString(c).toUpperCase();
         }
-        return str;
+        return newString;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(flipOuterCase("GrEaT"));
     }
 }
