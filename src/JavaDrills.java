9c90b6eadbf0012bee90aaf94d5ef4f9c946720e
CozyLaptop <albert.j.dominguez21@gmail.com>
2021-10-14 09:26:38 -0500

    unfinished work

---
+++ src/JavaDrills.java
@@ -0,0 +1,12 @@
+// Work in progress
+public class JavaDrills {
+    public static String flipOuterCase(String str){
+        for (int i = 0; i < str.length(); i++){
+            char c = str.charAt(i);
+            if (Character.isUpperCase(c)){
+                Character.toString();
+            }
+        }
+        return str;
+    }
+}
