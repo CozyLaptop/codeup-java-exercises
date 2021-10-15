81db5ed639fa498773b06a957fabc63e6979b95e
CozyLaptop <albert.j.dominguez21@gmail.com>
2021-10-15 09:25:23 -0500

    finished return total difference

--- src/JavaDrills.java
+++ src/JavaDrills.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 // Work in progress flip first and last character
 public class JavaDrills {
@@ -11,8 +14,22 @@ public class JavaDrills {
         }
         return newString;
     }
-
+    public static int returnTotalDifference(List<Integer> numbers1, List<Integer> numbers2)
+    {
+        int sumOfList1 = 0;
+        int sumOfList2 = 0;
+        for (int num: numbers1) {
+            sumOfList1 += num;
+        }
+        for (int num: numbers2) {
+            sumOfList2 += num;
+        }
+        return sumOfList1 - sumOfList2;
+    }
     public static void main(String[] args) {
         System.out.println(flipOuterCase("GrEaT"));
+        System.out.println(returnTotalDifference(Arrays.asList(10, 2, 3), Arrays.asList(1, 2, 3)));
+        System.out.println(returnTotalDifference(Arrays.asList(10, 1), Arrays.asList(1, 7)));
+        System.out.println(returnTotalDifference(Arrays.asList(10, 1), Arrays.asList(1, 7, 1)));
     }
 }
